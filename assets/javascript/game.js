7c3628c15db09006e36f63986bb8286667338c3d
Tayseer Samara <tayseersamara@dhcp-10-105-124-208.wireless.northwestern.private>
2018-02-23 10:02:16 -0600

    added files

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+
+var timer2 = "0:09";
+    var interval = setInterval(function() {
+  var timer = timer2.split(':');
+  //by parsing integer, I avoid all extra string processing
+  var minutes = parseInt(timer[0], 10);
+  var seconds = parseInt(timer[1], 10);
+  --seconds;
+  minutes = (seconds < 0) ? --minutes : minutes;
+  if (minutes < 0) clearInterval(interval);
+  seconds = (seconds < 0) ? 59 : seconds;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+  //minutes = (minutes < 10) ?  minutes : minutes;
+  $('#countdown').html(minutes + ':' + seconds);
+  timer2 = minutes + ':' + seconds;
+}, 1000);
